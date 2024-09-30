2a9c648f7aa81c6e1af0509d412b4df75042b9fb
kilinc89 <ibrhmklnc@gmail.com>
2024-09-30 23:52:08 +0300

    local storage added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './app/redux/store';
 
 import Navigation from './app/navigation/Navigation';
+import { loadFavorites } from './app/redux/slices/favoritesSlice';
 
 function App(): React.JSX.Element {
+
+  useEffect(() => {
+    store.dispatch(loadFavorites());
+  }, []);
+
   return (
     <>
       <Provider store={store}>
--- app/redux/slices/favoritesSlice.ts
+++ app/redux/slices/favoritesSlice.ts
@@ -1,5 +1,7 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { MovieDetail } from '../../types';
+import { storage } from '../../utils/storage';
+import { storageKeys } from '../../utils/storageKeys';
 
 interface FavoritesState {
   list: MovieDetail[];
@@ -9,19 +11,42 @@ const initialState: FavoritesState = {
   list: [],
 };
 
+export const loadFavorites = createAsyncThunk('favorites/loadFavorites', async () => {
+  try {
+    const favoritesString = storage.getString(storageKeys.FAVORITES);
+    if (favoritesString) {
+      const favorites = JSON.parse(favoritesString);
+      return favorites as MovieDetail[];
+    }
+    return [];
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+});
+
 const favoritesSlice = createSlice({
   name: 'favorites',
   initialState,
   reducers: {
     addFavorite(state, action: PayloadAction<MovieDetail>) {
       state.list.push(action.payload);
+      storage.set(storageKeys.FAVORITES, JSON.stringify(state.list));
     },
     removeFavorite(state, action: PayloadAction<{ imdbID: string }>) {
       state.list = state.list.filter((item) => item.imdbID !== action.payload.imdbID);
+      storage.set(storageKeys.FAVORITES, JSON.stringify(state.list));
     },
     setFavorites(state, action: PayloadAction<MovieDetail[]>) {
       state.list = action.payload;
+      storage.set(storageKeys.FAVORITES, JSON.stringify(state.list));
     },
+    
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loadFavorites.fulfilled, (state, action) => {
+      state.list = action.payload;
+    });
   },
 });
 
